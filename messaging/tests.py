63aa23830f1ae83d67006b0388b726b63dec30aa
lbrentcarpenter <garenarterius1@gmail.com>
2017-01-31 15:11:43 +0000

    set up structure for messaging tests

--- messaging/tests.py
+++ messaging/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Models
+from django.contrib.auth.models import User
+from users.models import Member
+from .models import *
+
+from users.views import spawn_user
+
+class Messaging(TestCase):
+	def setUp(self):
+		spawn_user('Alice', 'alice@tagarople.com', 'ySLLe8uy')
+		spawn_user('Bob', 'bob@tagarople.com', 'X3u9C4bp')
+		spawn_user('Eve', 'eve@shady.site', '5rX6zrQm')
+
+	def test_direct_message(self):
+		alice = User.objects.get(username='Alice')
+		bob = User.objects.get(username='Bob')
+		eve = User.objects.get(username='Eve')
+		# TODO: Tests
