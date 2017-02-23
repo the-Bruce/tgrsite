6a6714bcf0a7f8e13f1def02c6bde32cfab5af16
ash <garenarterius1@gmail.com>
2017-02-23 10:20:18 +0000

    messaging.models.send_to returns the message

--- messaging/views.py
+++ messaging/views.py
@@ -14,12 +14,12 @@ def send_message(member, thread, message):
 def send_to(sender, message, *pals_usernames):
 	pals = (Member.objects.get(equiv_user__username=x) for x in pals_usernames)
 	thread = MessageThread.get_thread(pals)
-	send_message(sender, thread, message)
+	return send_message(sender, thread, message)
 
 @login_required
 def dm(request):
 	# TODO: validate
-
+	#  ^ how??? ^
 	targets = request.POST.get('target').split(',')
 	targets = tuple(set(targets))
 	print(targets)
