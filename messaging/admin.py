59a4ff5cb50723570bef820938432f8979a0e536
lbrentcarpenter <garenarterius1@gmail.com>
2017-04-11 14:18:13 +0100

    make admins not see DMs

--- messaging/admin.py
+++ messaging/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 
 from .models import MessageThread, Message
 
-class MessageInline(admin.StackedInline):
-	model = Message
-	extra = 0
-class MessageThreadAdmin(admin.ModelAdmin):
-	inlines = [MessageInline]
+# these settings would give admins access to all DMs
+#class MessageInline(admin.StackedInline):
+#	model = Message
+#	extra = 0
+#class MessageThreadAdmin(admin.ModelAdmin):
+#	inlines = [MessageInline]
+#admin.site.register(MessageThread, MessageThreadAdmin)
+#admin.site.register(Message)
 
-admin.site.register(MessageThread, MessageThreadAdmin)
-admin.site.register(Message)
+# more sensible, though might still need to be removed
+admin.site.register(MessageThread)
