58bb7c5d0bd4ac79efaed38995d6cee667324318
ash <garenarterius1@gmail.com>
2017-04-21 17:33:15 +0100

    add larp section

--- statics/urls.py
+++ statics/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^schedule/', views.schedule, name='schedule'),
-	url(r'^gallery/', views.gallery, name='gallery'),
-
+	url(r'^schedule/$', views.schedule, name='schedule'),
+	url(r'^gallery/$', views.gallery, name='gallery'),
+	url(r'^larp/$', views.larp_info, name='larp_info'),
+	url(r'^larp/lore/$', views.larp_lore, name='larp_lore'),
 ]
