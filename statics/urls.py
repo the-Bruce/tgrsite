0f903a2cda1b2bd15f3b999ea7e3103fbadd7399
lbrentcarpenter <garenarterius1@gmail.com>
2017-05-12 14:52:10 +0100

    stuff?

--- statics/urls.py
+++ statics/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+# todo: apply this everywhere
+from django.views.generic import TemplateView
+
 urlpatterns = [
 	url(r'^schedule/$', views.schedule, name='schedule'),
 	url(r'^gallery/$', views.gallery, name='gallery'),
