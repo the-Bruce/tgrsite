6475cbb025c97c3c1e72618d0501bf53d51aa0fb
lbrentcarpenter <garenarterius1@gmail.com>
2017-05-12 18:07:26 +0100

    Change hotbar and URLs for 'RPGs' (games) and RPGs

--- rpgs/templatetags/rpg_tags.py
+++ rpgs/templatetags/rpg_tags.py
@@ -12,3 +12,28 @@ def can_manage(member, rpg):
 @register.assignment_tag(takes_context=True)
 def is_player(context, rpg):
 	return rpg.members.filter(id=context.request.user.member.id)
+
+# this is all one big horrible hack around the URL system......
+@register.simple_tag
+def add_tags(url, tag):
+	arr = tags_arr(url)
+	arr.append(tag)
+	arr = list(set(arr))
+	arr = get_non_tags(url) + arr
+	return '/'.join(arr)
+
+@register.simple_tag
+def tags_list(url):
+	arr = tags_arr(url)
+	r = ' / '.join(arr)
+	if r == '':
+		r = 'None'
+	return r
+
+def get_non_tags(url):
+	r = url.split('/')[1:3]
+	return r
+
+def tags_arr(url):
+	arr = url.split('/')[3:-1]
+	return arr
--- statics/urls.py
+++ statics/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r'^gallery/$', TemplateView.as_view(template_name='statics/gallery.html'), name='gallery'),
 	url(r'^larp/$', TemplateView.as_view(template_name='statics/larp_info.html'), name='larp_info'),
 	url(r'^larp/lore/$', TemplateView.as_view(template_name='statics/larp_lore.html'), name='larp_lore'),
+	url(r'^rpgs/$', TemplateView.as_view(template_name='statics/rpg_landing.html'), name='rpg_landing'),
 ]
