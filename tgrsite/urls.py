deb69f96d3e5a545acb37c8cdc9d1365f6794b48
lbrentcarpenter <garenarterius1@gmail.com>
2017-05-12 14:59:04 +0100

    make another template generic
    also rewrite comment about statics

--- tgrsite/urls.py
+++ tgrsite/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='homepage'),
+	url(r'^$', TemplateView.as_view(template_name='tgrsite/index.html'), name='homepage'),
 	url(r'^admin/', admin.site.urls),
 	url(r'^forum/', include('forum.urls')),
 	url(r'^rpg/', include('rpgs.urls')),
@@ -11,8 +12,9 @@ urlpatterns = [
 	url(r'^', include('users.urls')),
 	url(r'^messages/', include('messaging.urls')),
 	url(r'^bugs/', include('bugreports.urls')),
-	# static pages ish
-	# these will be located on the root path
+
+	# Pseudo "Static" pages - those with no models or fancy behaviour.
+	# e.g. the larp intro page
 	url(r'^', include('statics.urls')),
 
 ]
