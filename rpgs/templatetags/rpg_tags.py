dd8703dbab30482dc3388312116bdee27ba21c52
Louis Brent-Carpenter <garenarterius1@gmail.com>
2017-06-09 04:29:14 +0100

    rename rpg url to 'signups' and remove hardcoded urls

--- rpgs/templatetags/rpg_tags.py
+++ rpgs/templatetags/rpg_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.urls import reverse
 from rpgs.models import Rpg
 
 register = template.Library()
@@ -9,6 +10,15 @@ def can_manage(member, rpg):
 	# Not used as a tag - when this logic is changed be sure to update detail.html!
 	return (member == rpg.creator) or (member in list(rpg.game_masters.all()))
 
+# todo: assignment tag is deprecated
 @register.assignment_tag(takes_context=True)
 def is_player(context, rpg):
 	return rpg.members.filter(id=context.request.user.member.id)
+
+@register.simple_tag
+def rpg_url():
+	return reverse('rpg_tag', args=['rpg'])
+
+@register.simple_tag
+def rpgs_root():
+	return reverse('rpgs')
