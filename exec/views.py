3e1ed736ad9b4393a4bb407e295abc900140644d
lbrentcarpenter <garenarterius1@gmail.com>
2017-06-20 15:58:54 +0100

    implement sort order for exec list
    closes #5.

--- exec/views.py
+++ exec/views.py
@@ -4,6 +4,6 @@ from .models import ExecRole
 
 def index(request):
 	context = {
-		'execs': ExecRole.objects.all(),
+		'execs': ExecRole.objects.all().order_by('sort_order'),
 	}
 	return render(request, 'exec/index.html', context)
