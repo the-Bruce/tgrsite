05af8c0a4da6572fb6162b88e793c428022ac203
Ash Brent-Carpenter <ash@sent.com>
2017-10-02 00:45:13 +0100

    rename 'games signups' to 'events'

--- tgrsite/urls.py
+++ tgrsite/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from . import views
 
 urlpatterns = [
 	url(r'^$', TemplateView.as_view(template_name='tgrsite/index.html'), name='homepage'),
 	url(r'^admin/', admin.site.urls),
 	url(r'^forum/', include('forum.urls')),
-	url(r'^signups/', include('rpgs.urls')),
+
+	# hard redirect
+	# url(r'^signups/(.*)', RedirectView.as_view(url='/events/', permanent=True)),
+
+	# soft direct (/signups links still work but site universally links to /events)
+	# url(r'^signups/', include('rpgs.urls')),
+
+
+	url(r'^events/', include('rpgs.urls'), name='rpgs_root'),
 	url(r'^exec/', include('exec.urls')),
 	url(r'^', include('users.urls')),
 	url(r'^messages/', include('messaging.urls')),
