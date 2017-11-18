21a5406aa061ac2718625e33e9b6649d561f411d
ash <garenarterius1@gmail.com>
2017-11-18 15:44:09 +0000

    implement forum response anchoring

--- forum/urls.py
+++ forum/urls.py
@@ -5,11 +5,11 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='forum'),
 	url(r'^(?P<pk>[0-9]+)/$', views.forum, name='subforum'),
-	url(
-		r'^thread/(?P<pk>[0-9]+)/(?P<response_id>[0-9]+)/$',
-		views.thread,
-		name='viewresponse'
-	),
+	#url(
+	#	r'^thread/(?P<pk>[0-9]+)/#response-(?P<response_id>[0-9]+)/$',
+	#	views.thread,
+	#	name='viewresponse'
+	#),
 	url(
 		r'^thread/(?P<pk>[0-9]+)/$',
 		views.thread,
