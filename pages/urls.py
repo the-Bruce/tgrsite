2da5a4b1028609cac9ba074e9d089b881faa3de7
ashbc <ash@sent.com>
2018-03-25 14:47:02 +0100

    add route for generic pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
+	# TODO: convert this and all url() calls to path()
 	url(r'^$', views.page, {'name': 'index'}, name='homepage'),
+	path('page/<name>', views.page, name='page'),
 ]
