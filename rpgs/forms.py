192292010c2c4e7a471f16fcfaf22c97a5b7191b
Finnbar Keating <finnjkeating@aol.com>
2018-06-17 12:06:40 +0100

    Completed SimpleMDE work - added to events too.

--- rpgs/forms.py
+++ rpgs/forms.py
@@ -7,10 +7,12 @@ class RpgForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(RpgForm, self).__init__(*args, **kwargs)
 		for f in self.fields:
-			if f != 'am_i_gm' and f != 'is_in_the_past':
+			if f != 'am_i_gm' and f != 'is_in_the_past' and f != 'description':
 				self.fields[f].widget.attrs['class'] = 'form-control'
 			if f == 'is_in_the_past':
 				self.fields[f].label = 'Has this finished?'
+			if f == 'description':
+				self.fields[f].widget.attrs['class'] = 'markdown-input'
 	am_i_gm = forms.BooleanField(required=False)
 	am_i_gm.label = 'Are you running this?'
 	class Meta:
