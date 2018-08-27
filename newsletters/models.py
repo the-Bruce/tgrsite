582e82b5aec527553476ca155098a9f4d73597b1
Finnbar Keating <finnjkeating@aol.com>
2018-08-27 18:15:56 +0100

    Finished Newsletters

--- newsletters/models.py
+++ newsletters/models.py
@@ -3,15 +3,16 @@ from users.models import Member
 from django.urls import reverse
 
 class Newsletter(models.Model):
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
-    title = models.CharField(max_length=256)
-    body = models.TextField()
-    author = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True)
-    pub_date = models.DateTimeField('Date Posted', auto_now_add=True)
-    desc = models.CharField(max_length=256)
+	title = models.CharField(max_length=256)
+	body = models.TextField()
+	author = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True)
+	pub_date = models.DateTimeField('Date Posted', auto_now_add=True, null=True)
+	desc = models.CharField('Description', max_length=256)
+	ispublished = models.BooleanField('Is Published?')
 
-    # Important! If this changes, change the URL too!
-    def get_absolute_url(self):
-        return reverse('newsletters_detail', kwargs={'pk': self.id})
+# Important! If this changes, change the URL too!
+	def get_absolute_url(self):
+		return reverse('newsletters_detail', kwargs={'pk': self.id})
--- newsletters/views.py
+++ newsletters/views.py
@@ -10,21 +10,22 @@ from django.http import Http404
 
 class Index(generic.ListView):
 	model = Newsletter
-	ordering = ['-pub_date']
+	ordering = ['ispublished','-pub_date']
 
 class Detail(generic.DetailView):
 	model = Newsletter
+
+	def get_object(self, queryset=None):
+		obj = super(Detail, self).get_object()
+		if not obj.ispublished and not self.request.user.has_perm('newsletters.newsletters_edit'):
+			raise Http404
+		return obj
 
 class Create(PermissionRequiredMixin, generic.edit.CreateView):
 	model = Newsletter
 	form_class = NewsletterForm
 
 	permission_required = 'newsletters_create'
-
-	def form_valid(self, form):
-		form.instance.author = self.request.user.member
-		form.instance.pub_date = timezone.now()
-		return super(Create, self).form_valid(form)
 
 class Update(PermissionRequiredMixin, generic.edit.UpdateView):
 	model = Newsletter
@@ -33,10 +34,14 @@ class Update(PermissionRequiredMixin, generic.edit.UpdateView):
 	permission_required = 'newsletters_edit'
 	raise_exception = True
 
+	def form_valid(self, form):
+		if form.instance.ispublished: form.instance.pub_date = timezone.now()
+		return super(Update, self).form_valid(form)
+
 	def get_object(self, queryset=None):
 		""" Hook to ensure object is owned by request.user. """
 		obj = super(Update, self).get_object()
-		if not obj.author == self.request.user.member:
+		if obj.author and obj.author != self.request.user.member:
 			raise Http404
 		return obj
 
@@ -50,6 +55,6 @@ class Delete(PermissionRequiredMixin, generic.edit.DeleteView):
 	def get_object(self, queryset=None):
 		""" Hook to ensure object is owned by request.user. """
 		obj = super(Delete, self).get_object()
-		if obj.author != self.request.user.member:
+		if obj.author and obj.author != self.request.user.member:
 			raise Http404
 		return obj
