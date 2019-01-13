5afe12c8a7ec12724881952e0a1a336c143b750d
Finnbar Keating <finnjkeating@aol.com>
2019-01-13 19:28:29 +0000

    Tiny fixes.

--- pages/urls.py
+++ pages/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	# TODO: convert this and all url() calls to path()
 	url(r'^$', views.page, {'name': 'index'}, name='homepage'),
 	path('schedule', views.page, {'name': 'schedule'}, name='schedule'),
-	path('sponsorships', views.page, {'name': 'sponsorships}, name='sponsorships'),
+	path('sponsorships', views.page, {'name': 'sponsorships'}, name='sponsorships'),
 	path('page/<name>', views.page, name='page'),
 ]
