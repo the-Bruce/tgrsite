9ed8b0ebc4a7f8bbe42f3dc49e9996b8b019c699
thomas <44057980+the-Bruce@users.noreply.github.com>
2019-05-11 14:28:39 +0100

    Fix timetable changes add 6 extra blank events to admin view

--- timetable/admin.py
+++ timetable/admin.py
@@ -5,17 +5,34 @@ from .models import Timetable, Booking, Event, Week, ColourScheme
 
 # Register your models here.
 
-class BookingInline(admin.StackedInline):
+class NewBookingInline(admin.StackedInline):
     model = Booking
     fields = ['room', 'event']
     extra = 6
 
+
+class ChangeBookingInline(admin.StackedInline):
+    model = Booking
+    fields = ['room', 'event']
+    extra = 0
+
+
 class WeekAdmin(admin.ModelAdmin):
     model = Week
-    inlines = [BookingInline]
+    inlines = []
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        self.inlines = [ChangeBookingInline]
+        return super().change_view(request, object_id, form_url, extra_context)
+
+    def add_view(self, request, form_url='', extra_context=None):
+        self.inlines = [NewBookingInline]
+        return super().add_view(request, form_url, extra_context)
+
 
 class TimetableAdmin(admin.ModelAdmin):
     model = Timetable
+
 
 admin.site.register(Event)
 admin.site.register(Week, WeekAdmin)
