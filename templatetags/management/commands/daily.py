932754aa09a91a7188c897f3768e447a438cb556
thomas <44057980+the-Bruce@users.noreply.github.com>
2019-09-15 22:28:17 +0100

    Add cleanmessages to daily task

--- templatetags/management/commands/daily.py
+++ templatetags/management/commands/daily.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Count
 
 from notifications.tasks import doSummaryNotificationMailings
+from messaging.models import MessageThread
 
 
 class Command(BaseCommand):
+    help = 'Performs all daily cleanup tasks'
+
     def handle(self, *args, **options):
         doSummaryNotificationMailings()
+        MessageThread.objects.annotate(Count("message")).filter(message__count=0).all().delete()
+        self.stdout.write('Messages Thread Cleaned')
