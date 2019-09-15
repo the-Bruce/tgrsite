13d4e6ce6520e419c15da9448a1c1c3d036b4831
thomas <44057980+the-Bruce@users.noreply.github.com>
2019-09-15 19:18:38 +0100

    Fix #150 and #151 Note new requirement on bleach as tag escaping was no longer enough. Added css to actually render blockquotes as something

--- templatetags/templatetags/markdown_tags.py
+++ templatetags/templatetags/markdown_tags.py
@@ -3,6 +3,7 @@ from django.templatetags import static
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from markdown import markdown
+from bleach.sanitizer import Cleaner
 
 register = template.Library()
 exts = ['markdown.extensions.nl2br', 'pymdownx.caret', 'pymdownx.tilde', 'sane_lists']
@@ -10,7 +11,8 @@ exts = ['markdown.extensions.nl2br', 'pymdownx.caret', 'pymdownx.tilde', 'sane_l
 
 @register.filter(is_safe=True)
 def parse_md(value):
-    return mark_safe(markdown(escape(value), extensions=exts, output_format='html5'))
+    return split_escape_after(markdown(split_escape_before(value), extensions=exts, output_format='html5'))
+
 
 # Parses markdown WITHOUT escaping. Use with caution!
 @register.filter(is_safe=True)
@@ -27,3 +29,28 @@ class FullStaticNode(static.StaticNode):
 @register.tag('fullstatic')
 def do_static(parser, token):
     return FullStaticNode.handle_token(parser, token)
+
+
+_html_escapes_before = {
+    ord('&'): '&amp;',
+    ord('<'): '&lt;',
+    # ord('>'): '&gt;', # Potentially risky, using bleach to help mitigate this, but allows blockquotes
+}
+
+_html_escapes_after = {
+    ord('"'): '&quot;',
+    ord("'"): '&#39;',
+}
+
+
+def split_escape_before(text):
+    return str(text).translate(_html_escapes_before)
+
+
+def split_escape_after(text):
+    text = str(text).translate(_html_escapes_after)
+    cleaner = Cleaner(tags=['p', 'br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'em', 'strong', 'a', 'ul', 'ol', 'li',
+                            'blockquote', 'img', 'pre', 'code', 'hr'],
+                      attributes={'a': ['href']}, protocols=['http', 'https'])
+    text = cleaner.clean(text)
+    return mark_safe(text)
