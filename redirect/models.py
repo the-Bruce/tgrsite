ce1ff84d8f36ec8f42c87680e3bf0383688d0703
thomas <44057980+the-Bruce@users.noreply.github.com>
2019-09-18 13:44:36 +0100

    Fix #155

--- redirect/models.py
+++ redirect/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Redirect(models.Model):
     source = models.CharField(max_length=30)
-    sink = models.CharField(max_length=50)
+    sink = models.CharField(max_length=250)
     permanent = models.BooleanField(default=False)
 
     def __str__(self):
