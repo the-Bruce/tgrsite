3cfbb82de64da35cc16628d633555dc9e8d90c7b
thomas <44057980+the-Bruce@users.noreply.github.com>
2019-09-29 15:50:26 +0100

    Add a quick subscribe link for the newsletter.

--- notifications/urls.py
+++ notifications/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('clear/', views.ReadAll.as_view(), name='clear_notifications'),
     path('read/<int:pk>/', views.ReadNotification.as_view(), name='read_notification'),
     path('preferences/', views.UpdateSubscriptions.as_view(), name='notification_settings'),
-    path('temp/', views.email_notifications)
+    path('temp/', views.email_notifications),
+    path('newsletter_subscribe/', views.quick_newsletter_subscribe, name='quick_newsletter_subscribe'),
 ]
