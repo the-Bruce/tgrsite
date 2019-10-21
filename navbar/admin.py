9c58a81d4169ed9a6807b8ad13a2e9b66fe255ec
thomas <44057980+the-Bruce@users.noreply.github.com>
2019-10-21 22:11:03 +0100

    Some admin site cleanup

--- navbar/admin.py
+++ navbar/admin.py
@@ -7,13 +7,15 @@ class DropdownList(StackedInline):
     model = DropDownItem
     extra = 1
 
+class BarItemAdmin(ModelAdmin):
+    list_display = ("text", "sort_index", "target")
 
 class BarDropdownAdmin(ModelAdmin):
     model = BarDropdown
     inlines = [DropdownList]
+    list_display = ("text", "sort_index")
 
 
 # Register your models here.
-admin.site.register(BarItem)
+admin.site.register(BarItem, BarItemAdmin)
 admin.site.register(BarDropdown, BarDropdownAdmin)
-#admin.site.register(DropDownItem)
