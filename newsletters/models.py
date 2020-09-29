50eb866c78557c4ef3c77313ff7dabf7739a43db
thomas <44057980+the-Bruce@users.noreply.github.com>
2020-09-29 23:16:53 +0100

    Allow changeable newsletter banners

--- newsletters/models.py
+++ newsletters/models.py
@@ -5,12 +5,18 @@ from users.models import Member
 
 
 class Newsletter(models.Model):
+    BANNERS = (
+        ("newsletter-banner.png", "Older"),
+        ("images/newsletter_banner_2.png", "2019-20"),
+        ("images/newsletter_banner_3.png", "2020-21"),
+    )
     title = models.CharField(max_length=256)
     body = models.TextField()
     author = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True)
     pub_date = models.DateTimeField('Date Posted', auto_now_add=True)
     summary = models.CharField('Summary', max_length=256)
     ispublished = models.BooleanField('Is Published?')
+    banner = models.CharField(max_length=255, choices=BANNERS, default=BANNERS[-1][0])
 
     def __str__(self):
         return self.title
