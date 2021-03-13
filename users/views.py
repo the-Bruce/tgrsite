444a1076de602fde3a2bf47f26961de16ca1dee5
Finnbar Keating <finnjkeating@gmail.com>
2021-03-13 20:38:17 +0000

    Added a nice user-side achievements view. Lovely!

--- users/views.py
+++ users/views.py
@@ -34,7 +34,8 @@ class ProfileView(LoginRequiredMixin, DetailView):
         ctxt = super().get_context_data(**kwargs)
         ctxt.update({'recent_threads': Thread.objects.filter(author__id=pk).order_by('-pub_date')[:3],
                      'recent_responses': Response.objects.filter(author__id=pk).order_by('-pub_date')[:3],
-                     'rpgs': Rpg.objects.filter(game_masters__id=pk, is_in_the_past=False)})
+                     'rpgs': Rpg.objects.filter(game_masters__id=pk, is_in_the_past=False),
+                     'achievements': self.object.achievement_set.values()})
         return ctxt
 
 
