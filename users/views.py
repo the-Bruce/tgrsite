6379897b2f093b2e06c4e560cdc010863b63258e
Finnbar Keating <finnjkeating@gmail.com>
2021-03-13 22:44:42 +0000

    Now date of achievement is shown with the achievement.

--- users/views.py
+++ users/views.py
@@ -35,7 +35,7 @@ class ProfileView(LoginRequiredMixin, DetailView):
         ctxt.update({'recent_threads': Thread.objects.filter(author__id=pk).order_by('-pub_date')[:3],
                      'recent_responses': Response.objects.filter(author__id=pk).order_by('-pub_date')[:3],
                      'rpgs': Rpg.objects.filter(game_masters__id=pk, is_in_the_past=False),
-                     'achievements': self.object.achievement_set.all()})
+                     'achievements': self.object.achievementaward_set.all()})
         return ctxt
 
 
