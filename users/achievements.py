900b2be6b100e87f1af79d1cc9e354aacc81cd53
Finnbar Keating <finnjkeating@gmail.com>
2021-03-18 09:30:30 +0000

    Use timezone.now instead of datetime.now

--- users/achievements.py
+++ users/achievements.py
@@ -2,7 +2,7 @@ from users.models import Member, Achievement, AchievementAward
 from notifications.utils import notify
 from notifications.models import NotifType
 
-from datetime import datetime, timezone
+from django.utils import timezone
 
 def give_achievement(member : Member, trigger : str):
     achiev, _ = Achievement.objects.get_or_create(
@@ -32,8 +32,9 @@ age_awards = {1: "one_year", 2: "two_years", 3: "three_years", 4: "four_years", 
 
 def age_achievements(member : Member):
     date = member.equiv_user.date_joined
-    timesince = datetime.now(timezone.utc) - date
+    timesince = timezone.now() - date
     # This will be inaccurate every 400 years or something, so is good enough.
+    # The alternative is adding a new dependency.
     years = timesince.days / 365.2425
     for i in age_awards.keys():
         if years >= i:
