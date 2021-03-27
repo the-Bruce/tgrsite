74310ffaf6798594ee914e8db1e5c35a518ddd92
Finnbar Keating <finnjkeating@gmail.com>
2021-03-27 22:34:37 +0000

    Events with coin requirements, fixed middleware Middleware was firing multiple times per page due to JSON requests

--- tgrsite/middleware.py
+++ tgrsite/middleware.py
@@ -3,6 +3,7 @@ from users.achievements import give_achievement_once
 from random import randint
 from django.contrib.messages import add_message
 from django.contrib.messages import constants as messages
+from django.http import JsonResponse
 
 def tabletopcoin_algo():
     return randint(0, 100)
@@ -12,7 +13,8 @@ def tabletopcoin_uncertainty():
 
 def mystery_middleware(get_response):
     def middleware(request):
-        if request.user.is_authenticated:
+        response = get_response(request)
+        if not isinstance(response, JsonResponse) and response.status_code == 200 and request.method == 'GET' and request.user.is_authenticated:
             # Perform the extremely complex TabletopCoin algorithm
             action_worth = tabletopcoin_algo()
             uncertainty = tabletopcoin_uncertainty()
@@ -22,12 +24,11 @@ def mystery_middleware(get_response):
                 add_message(request, messages.SUCCESS,
                     f"You earned {coins_won} TabletopCoin for interacting with the website!")
             elif 0 <= action_worth <= 20:
-                coins_lost = uncertainty + action_worth * 2
+                coins_lost = uncertainty + 1 + action_worth * 2
                 request.user.member.award_coin(-coins_lost)
                 add_message(request, messages.ERROR,
                     f"You lost {coins_lost} TabletopCoin for interacting badly with the website.")
             if request.user.member.coin() >= 1000:
                 give_achievement_once(request.user.member, "moneybags")
-        response = get_response(request)
         return response
     return middleware
