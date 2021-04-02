5ca28127115daa352637f89d286c2f41548401fd
Finnbar Keating <finnjkeating@gmail.com>
2021-04-02 10:55:19 +0100

    Fixed redirect w/ achievement bug when logged out

--- redirect/views.py
+++ redirect/views.py
@@ -10,7 +10,7 @@ def redirect(request, source):
     sink = get_object_or_404(Redirect, source__iexact=source)
     sink.usages += 1
     sink.save()
-    if sink.achievement:
+    if sink.achievement and not request.user.is_anonymous:
         give_this_achievement_once(request.user.member, sink.achievement, request=request)
     if sink.permanent:
         return HttpResponsePermanentRedirect(sink.sink)
